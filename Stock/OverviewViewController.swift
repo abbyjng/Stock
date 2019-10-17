37f03656e491d52d4783254eb2a28e211002d17b
ajng <ajng@umich.edu>
2019-10-17 00:58:26 -0400

    began working on UI

---
+++ Stock/OverviewViewController.swift
@@ -0,0 +1,30 @@
+//
+//  OverviewViewController.swift
+//  Stock
+//
+//  Created by Abigail Ng on 10/17/19.
+//  Copyright © 2019 Abigail Ng. All rights reserved.
+//
+
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
