9d88aa3cb8750e1c9c97c54960c17a666ba4aefd
ajng <ajng@umich.edu>
2019-10-16 23:36:46 -0400

    initial commit

---
+++ Stock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Stock
+//
+//  Created by Abigail Ng on 10/16/19.
+//  Copyright © 2019 Abigail Ng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StockTests/StockTests.swift
@@ -0,0 +1,34 @@
+//
+//  StockTests.swift
+//  StockTests
+//
+//  Created by Abigail Ng on 10/16/19.
+//  Copyright © 2019 Abigail Ng. All rights reserved.
+//
+
+import XCTest
+@testable import Stock
+
+class StockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
