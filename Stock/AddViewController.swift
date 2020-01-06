b176548355788e54779e8efdd2d8fe43c5d78b3d
ajng <ajng@umich.edu>
2020-01-06 13:57:31 -0500

    continued to add capabilites to edit and add more items

---
+++ Stock/AddViewController.swift
@@ -0,0 +1,30 @@
+//
+//  AddViewController.swift
+//  Stock
+//
+//  Created by Abigail Ng on 12/20/19.
+//  Copyright © 2019 Abigail Ng. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
